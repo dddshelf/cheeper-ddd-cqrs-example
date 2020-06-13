23f1c13284d8e337a494f3c86c6bc5eb40df7a0b
theUniC <theunic@gmail.com>
2020-06-13 13:25:36 +0200

    Added Bulma CSS Framework. Made homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
