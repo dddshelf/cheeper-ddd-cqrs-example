291b765f46c552d767cbd81099f1000e87a15cda
Keyvan Akbary <me@keyvanakbary.com>
2020-07-24 12:38:32 +0200

    Add Command Handler examples

--- tests/Cheeper/Tests/Application/Command/Author/SignUpHandlerTest.php
+++ tests/Cheeper/Tests/Application/Command/Author/SignUpHandlerTest.php
@@ -5,61 +5,77 @@ declare(strict_types=1);
 namespace Cheeper\Tests\Application\Command\Author;
 
 use Cheeper\DomainModel\Author\AuthorAlreadyExists;
+use Cheeper\DomainModel\Author\Authors;
 use Cheeper\DomainModel\Author\UserName;
-use Cheeper\Tests\Helper\SendsCommands;
-use DateTimeImmutable;
+use Cheeper\Application\Command\Author\SignUp;
+use Cheeper\Application\Command\Author\SignUpHandler;
+use Cheeper\Infrastructure\Persistence\InMemoryAuthors;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 
 final class SignUpHandlerTest extends TestCase
 {
-    use SendsCommands;
-
     /** @test */
     public function givenAUserNameThatAlreadyBelongsToAnExistingUserWhenSignUpThenAnExceptionShouldBeThrown(): void
     {
         $this->expectException(AuthorAlreadyExists::class);
-        $this->expectExceptionMessage('Author with name "test" already exists');
+        $this->expectExceptionMessage('Author with name "johndoe" already exists');
 
-        $this->signUpAuthorWith(
-            Uuid::uuid4()->toString(),
-            'test',
-            'test@email.com',
-            'test',
-            'test',
-            'test',
-            'https://google.com/',
-            (new DateTimeImmutable())->format('Y-m-d')
+        $authors = new InMemoryAuthors();
+
+        //snippet sign-up-handler-usage
+        $signUpHandler = new SignUpHandler($authors);
+
+        $signUpHandler(
+            new SignUp(
+                Uuid::uuid4()->toString(),
+                'johndoe',
+                'johndoe@example.com',
+                'John Doe',
+                'The usual profile example',
+                'Madrid',
+                'https://example.com/',
+                (new \DateTimeImmutable())->format('Y-m-d')
+            )
         );
+        //end-snippet
 
-        $this->signUpAuthorWith(
-            Uuid::uuid4()->toString(),
-            'test',
-            'test@email.com',
-            'test',
-            'test',
-            'test',
-            'https://google.com/',
-            (new DateTimeImmutable())->format('Y-m-d')
+        $signUpHandler(
+            new SignUp(
+                Uuid::uuid4()->toString(),
+                'johndoe',
+                'johndoe@example.com',
+                'John Doe',
+                'The usual profile example',
+                'Madrid',
+                'https://example.com/',
+                (new \DateTimeImmutable())->format('Y-m-d')
+            )
         );
     }
 
     /** @test */
     public function givenValidUserDataWhenSignUpThenAValidUserShouldBeCreated(): void
     {
-        $this->signUpAuthorWith(
-            Uuid::uuid4()->toString(),
-            'test',
-            'test@email.com',
-            'test',
-            'test',
-            'test',
-            'https://google.com/',
-            (new DateTimeImmutable())->format('Y-m-d')
+        $authors = new InMemoryAuthors();
+
+        $signUpHandler = new SignUpHandler($authors);
+
+        $signUpHandler(
+            new SignUp(
+                Uuid::uuid4()->toString(),
+                'johndoe',
+                'johndoe@example.com',
+                'John Doe',
+                'The usual profile example',
+                'Madrid',
+                'https://example.com/',
+                (new \DateTimeImmutable())->format('Y-m-d')
+            )
         );
 
         $this->assertNotNull(
-            $this->authors->ofUserName(UserName::pick('test'))
+            $authors->ofUserName(UserName::pick('johndoe'))
         );
     }
 }
