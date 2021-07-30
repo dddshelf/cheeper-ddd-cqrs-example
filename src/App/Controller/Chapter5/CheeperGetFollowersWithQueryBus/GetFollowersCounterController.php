a9393411fcded52ed5a6f1728b19657fea223fa3
Carlos Buenosvinos <carlos.buenosvinos@gmail.com>
2021-07-30 20:38:25 +0200

    Chapter 5 Query Bus part

--- src/App/Controller/Chapter5/CheeperGetFollowersWithQueryBus/GetFollowersCounterController.php
+++ src/App/Controller/Chapter5/CheeperGetFollowersWithQueryBus/GetFollowersCounterController.php
@@ -5,41 +5,54 @@ declare(strict_types=1);
 namespace App\Controller\Chapter5\CheeperGetFollowersWithQueryBus;
 
 use Cheeper\Chapter5\Application\Query\CountFollowers;
-use Cheeper\Chapter5\DomainModel\Follow\FollowersCounterResource;
-use Cheeper\DomainModel\Author\AuthorDoesNotExist;
-use Cheeper\DomainModel\Author\AuthorId;
+use Cheeper\Chapter5\Application\Query\QueryBus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+// snippet get-followers-counter-controller
 final class GetFollowersCounterController extends AbstractController
 {
-    public function __construct(
-        private QueryBus $queryBus
-    )
+    public function __construct(private QueryBus $queryBus)
     {
     }
 
-    #[Route("/chapter-5/api/followers-counter/using-query-bus/{userId}", methods: ["GET"])]
-    public function __invoke(string $userId): JsonResponse
+    #[Route("/chapter-5/api/get-followers-counter/using-query-bus/{authorId}", methods: ["GET"])]
+    public function __invoke(string $authorId): JsonResponse
     {
-        $authorId = AuthorId::fromString($userId);
-
-        try {
-            return new JsonResponse(
-                $this->queryBus->query(
-                    CountFollowers::ofAuthor($authorId)
-                ),
-                Response::HTTP_OK
+        return
+            $this->json(
+                $this
+                    ->queryBus
+                    ->query(
+                        CountFollowers::ofAuthor($authorId)
+                    ),
             );
-        } catch (AuthorDoesNotExist $e) {
-            $jsonResponse = new JsonResponse($e, Response::HTTP_NOT_FOUND);
-        } catch (\Exception $e) {
-            $jsonResponse = new JsonResponse($e, Response::HTTP_NOT_FOUND);
-        }
 
-        return $jsonResponse;
+        /**
+         * In case of errors like the author does not exist, you can:
+         * A) Let your framework catch the Exceptions and automatically
+         *    generate 404 o 500
+         * B) Do it manually here in the Controller, using a try block
+         *    and multiple catch blocks
+         *
+         *    try {
+         *        ...
+         *    } catch (AuthorDoesNotExist $e) {
+         *        $jsonResponse = new JsonResponse(
+         *            $e->getMessage(),
+         *            Response::HTTP_NOT_FOUND
+         *        );
+         *    } catch (\Exception $e) {
+         *        $jsonResponse = new JsonResponse(
+         *            $e->getMessage(),
+         *            Response::HTTP_INTERNAL_SERVER_ERROR
+         *        );
+         *    }
+         *    ...
+         *
+         *    return $jsonResponse;
+         */
     }
 }
+//end-snippet
