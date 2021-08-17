047423e1d2fa8bc967eefd815afbb7283a26c2ea
Carlos Buenosvinos <carlos.buenosvinos@gmail.com>
2021-08-17 13:38:40 +0200

    Chapter6: Closing the Loop

--- src/Cheeper/Chapter6/Infrastructure/Application/Projector/Author/SymfonyCountFollowerProjector.php
+++ src/Cheeper/Chapter6/Infrastructure/Application/Projector/Author/SymfonyCountFollowerProjector.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Cheeper\Chapter6\Infrastructure\Application\Projector\Author;
 
+use Cheeper\Chapter6\Application\Projector\Author\CountFollowerProjector;
 use Cheeper\Chapter6\Application\Projector\Author\CountFollowers;
+use Cheeper\DomainModel\Follow\AuthorFollowed;
 use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
 
 //snippet symfony-projector-count-followers
@@ -14,14 +16,16 @@ final class SymfonyCountFollowerProjector implements MessageSubscriberInterface
         private CountFollowerProjector $appProjector
     ) { }
 
-    public function __invoke(CountFollowers $query): void
+    public function __invoke(AuthorFollowed $event): void
     {
-        $this->appProjector($query);
+        $this->appProjector->__invoke(
+            CountFollowers::ofAuthor($event->toAuthorId())
+        );
     }
 
     public static function getHandledMessages(): iterable
     {
-        yield CountFollowers::class => [
+        yield AuthorFollowed::class => [
             'bus' => 'event.bus'
         ];
     }
